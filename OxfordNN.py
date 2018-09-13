8cfb810d63367aab091cc178db470b5a4370a6d6
ofirbarak <ofirbirka@gmail.com>
2018-09-13 21:56:31 +0300

    balh

---
+++ OxfordNN.py
@@ -0,0 +1,103 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+from vgg_17_keras import *
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+from oxford_classes import classes
+import os, cv2
+
+
+# In[2]:
+
+
+def get_data(pics_folder_path, num_classes):
+    X, Y = [],[]
+    names = os.listdir(pics_folder_path)
+    for i,n in enumerate(names):
+        im = cv2.imread(pics_folder_path+n)
+        resized_im = cv2.resize(im, (224, 224)).astype(np.float32)
+        resized_im = (resized_im - np.min(resized_im))/(np.max(resized_im)-np.min(resized_im))
+        resized_im = resized_im.transpose((2,0,1))
+
+        X.append(resized_im)
+
+        y_ = np.zeros(num_classes)
+        im_class = n[:n.rfind('_')]
+        index = 17
+        if im_class in classes:
+            index = classes.index(im_class)
+        y_[index] = 1
+        Y.append(y_)
+
+        #delete this!!
+        if i == 99:
+            break
+    
+    X = np.stack(X, axis=0)
+    Y = np.stack(Y, axis=0)
+    return X,Y
+    
+    
+    
+
+
+# In[3]:
+
+
+path = 'oxbuild_images/'
+num_classes = 18
+
+X,Y = get_data(path, num_classes)
+X.shape, Y.shape
+
+
+# In[4]:
+
+
+# divide data
+num_examples = int(X.shape[0])
+num_tests = int(0.8*num_examples)
+mask = np.full(X.shape[0], False)
+mask[:num_tests] = True
+np.random.shuffle(mask)
+
+X_train = X[mask]
+Y_train = Y[mask]
+X_test = X[np.invert(mask)]
+Y_test = Y[np.invert(mask)]
+
+
+# In[5]:
+
+
+model=base_model()
+
+
+# In[6]:
+
+
+# model.predict(X_train[0])
+im = X_train[:10]
+# im = im.transpose((2,0,1))
+# im = np.expand_dims(im, axis=0)
+# y_ = Y_train[:10]
+# print(y_.shape)
+# im.shape, y_.shape
+y_ = model.predict(im)
+print(im.shape, y_.shape)
+model.fit(im, y_, epochs=1, batch_size=5)
+print("end")
+
+
+# In[ ]:
+
+
+# model = train_model(model, X_train, Y_train)
+# y = model.predict(X_test)
+# print(np.count(y==Y_test)/Y_test.shape[0])
--- vgg_17_keras.py
+++ vgg_17_keras.py
@@ -45,87 +45,87 @@ def base_model():
     model.add(BatchNormalization()) if BATCH_NORM else None
     model.add(Activation('relu'))
 
-#     model.add(Conv2D(64, (3, 3), padding='same', name='block1_conv2'))
-#     model.add(BatchNormalization()) if BATCH_NORM else None
-#     model.add(Activation('relu'))
+    model.add(Conv2D(64, (3, 3), padding='same', name='block1_conv2'))
+    model.add(BatchNormalization()) if BATCH_NORM else None
+    model.add(Activation('relu'))
 
-#     model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool'))
-
-#     model.add(Conv2D(128, (3, 3), padding='same', name='block2_conv1'))
-#     model.add(BatchNormalization()) if BATCH_NORM else None
-#     model.add(Activation('relu'))
-
-#     model.add(Conv2D(128, (3, 3), padding='same', name='block2_conv2'))
-#     model.add(BatchNormalization()) if BATCH_NORM else None
-#     model.add(Activation('relu'))
-#     model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool'))
-
-#     model.add(Conv2D(256, (3, 3), padding='same', name='block3_conv1'))
-#     model.add(BatchNormalization()) if BATCH_NORM else None
-#     model.add(Activation('relu'))
-
-#     model.add(Conv2D(256, (3, 3), padding='same', name='block3_conv2'))
-#     model.add(BatchNormalization()) if BATCH_NORM else None
-#     model.add(Activation('relu'))
-
-#     model.add(Conv2D(256, (3, 3), padding='same', name='block3_conv3'))
-#     model.add(BatchNormalization()) if BATCH_NORM else None
-#     model.add(Activation('relu'))
-
-#     model.add(Conv2D(256, (3, 3), padding='same', name='block3_conv4'))
-#     model.add(BatchNormalization()) if BATCH_NORM else None
-#     model.add(Activation('relu'))
-
-#     model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool'))
-
-#     model.add(Conv2D(512, (3, 3), padding='same', name='block4_conv1'))
-#     model.add(BatchNormalization()) if BATCH_NORM else None
-#     model.add(Activation('relu'))
-
-#     model.add(Conv2D(512, (3, 3), padding='same', name='block4_conv2'))
-#     model.add(BatchNormalization()) if BATCH_NORM else None
-#     model.add(Activation('relu'))
-
-#     model.add(Conv2D(512, (3, 3), padding='same', name='block4_conv3'))
-#     model.add(BatchNormalization()) if BATCH_NORM else None
-#     model.add(Activation('relu'))
-
-#     model.add(Conv2D(512, (3, 3), padding='same', name='block4_conv4'))
-#     model.add(BatchNormalization()) if BATCH_NORM else None
-#     model.add(Activation('relu'))
-#     model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool'))
-
-#     model.add(Conv2D(512, (3, 3), padding='same', name='block5_conv1'))
-#     model.add(BatchNormalization()) if BATCH_NORM else None
-#     model.add(Activation('relu'))
-
-#     model.add(Conv2D(512, (3, 3), padding='same', name='block5_conv2'))
-#     model.add(BatchNormalization()) if BATCH_NORM else None
-#     model.add(Activation('relu'))
-
-#     model.add(Conv2D(512, (3, 3), padding='same', name='block5_conv3'))
-#     model.add(BatchNormalization()) if BATCH_NORM else None
-#     model.add(Activation('relu'))
-
-#     model.add(Conv2D(512, (3, 3), padding='same', name='block5_conv4'))
-#     model.add(BatchNormalization()) if BATCH_NORM else None
-#     model.add(Activation('relu'))
-
-#     model.add(Flatten())
-
-#     model.add(Dense(4096))
-#     model.add(BatchNormalization()) if BATCH_NORM else None
-#     model.add(Activation('relu'))
-#     model.add(Dropout(0.5))
-
-#     model.add(Dense(4096, name='fc2'))
-#     model.add(BatchNormalization()) if BATCH_NORM else None
-#     model.add(Activation('relu'))
-#     model.add(Dropout(0.5))
-
-#     model.add(Dense(num_classes))
-#     model.add(BatchNormalization()) if BATCH_NORM else None
-#     model.add(Activation('softmax'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool'))
+    #
+    # model.add(Conv2D(128, (3, 3), padding='same', name='block2_conv1'))
+    # model.add(BatchNormalization()) if BATCH_NORM else None
+    # model.add(Activation('relu'))
+    #
+    # model.add(Conv2D(128, (3, 3), padding='same', name='block2_conv2'))
+    # model.add(BatchNormalization()) if BATCH_NORM else None
+    # model.add(Activation('relu'))
+    # model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool'))
+    #
+    # model.add(Conv2D(256, (3, 3), padding='same', name='block3_conv1'))
+    # model.add(BatchNormalization()) if BATCH_NORM else None
+    # model.add(Activation('relu'))
+    #
+    # model.add(Conv2D(256, (3, 3), padding='same', name='block3_conv2'))
+    # model.add(BatchNormalization()) if BATCH_NORM else None
+    # model.add(Activation('relu'))
+    #
+    # model.add(Conv2D(256, (3, 3), padding='same', name='block3_conv3'))
+    # model.add(BatchNormalization()) if BATCH_NORM else None
+    # model.add(Activation('relu'))
+    #
+    # model.add(Conv2D(256, (3, 3), padding='same', name='block3_conv4'))
+    # model.add(BatchNormalization()) if BATCH_NORM else None
+    # model.add(Activation('relu'))
+    #
+    # model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool'))
+    #
+    # model.add(Conv2D(512, (3, 3), padding='same', name='block4_conv1'))
+    # model.add(BatchNormalization()) if BATCH_NORM else None
+    # model.add(Activation('relu'))
+    #
+    # model.add(Conv2D(512, (3, 3), padding='same', name='block4_conv2'))
+    # model.add(BatchNormalization()) if BATCH_NORM else None
+    # model.add(Activation('relu'))
+    #
+    # model.add(Conv2D(512, (3, 3), padding='same', name='block4_conv3'))
+    # model.add(BatchNormalization()) if BATCH_NORM else None
+    # model.add(Activation('relu'))
+    #
+    # model.add(Conv2D(512, (3, 3), padding='same', name='block4_conv4'))
+    # model.add(BatchNormalization()) if BATCH_NORM else None
+    # model.add(Activation('relu'))
+    # model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool'))
+    #
+    # model.add(Conv2D(512, (3, 3), padding='same', name='block5_conv1'))
+    # model.add(BatchNormalization()) if BATCH_NORM else None
+    # model.add(Activation('relu'))
+    #
+    # model.add(Conv2D(512, (3, 3), padding='same', name='block5_conv2'))
+    # model.add(BatchNormalization()) if BATCH_NORM else None
+    # model.add(Activation('relu'))
+    #
+    # model.add(Conv2D(512, (3, 3), padding='same', name='block5_conv3'))
+    # model.add(BatchNormalization()) if BATCH_NORM else None
+    # model.add(Activation('relu'))
+    #
+    # model.add(Conv2D(512, (3, 3), padding='same', name='block5_conv4'))
+    # model.add(BatchNormalization()) if BATCH_NORM else None
+    # model.add(Activation('relu'))
+    #
+    # model.add(Flatten())
+    #
+    # model.add(Dense(4096))
+    # model.add(BatchNormalization()) if BATCH_NORM else None
+    # model.add(Activation('relu'))
+    # model.add(Dropout(0.5))
+    #
+    # model.add(Dense(4096, name='fc2'))
+    # model.add(BatchNormalization()) if BATCH_NORM else None
+    # model.add(Activation('relu'))
+    # model.add(Dropout(0.5))
+    #
+    # model.add(Dense(num_classes))
+    # model.add(BatchNormalization()) if BATCH_NORM else None
+    # model.add(Activation('softmax'))
 
     sgd = SGD(lr=0.0005, decay=0, nesterov=True)
 
